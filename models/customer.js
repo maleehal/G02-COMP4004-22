88a8c2a02a56833950170aa5b7fe7dd428dba040
eshen22 <cb010517@students.apiit.lk>
2022-12-20 21:28:37 +0530

    Completed customer user model

---
+++ models/customer.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+
+const customerSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"this field is required"],
+        
+
+    },
+    username:{
+        type:String,
+        required:[true,"this field is required"],
+        unique:true,
+        lowercase:true,
+
+        
+
+    },
+    email:{
+        type:String,
+        required:[true,"this field is required"],
+        unique:true,
+        lowercase:true,
+        validate:[isEmail ,"please enter a valid email address" ]
+    },
+    password:{
+        required:[true,"this field is required"],
+        type:String,
+        minlength:[6 ,"minimum 6 characters required"]
+    },
+    profilepic:{
+        type:Buffer,
+    }
+})
+
+module.exports = mongoose.model("Customer",customerSchema)
