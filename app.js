aa7643e64fed212c45387533f3d0dc1863258d26
eshen22 <cb010517@students.apiit.lk>
2022-12-21 16:19:09 +0530

    coomit

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use('/api/v1/customer', customerRoutes);
 const start = async () => {
     try {
       await connectDB(process.env.MONGO_URI);
+    
       app.listen(port, () =>
         console.log(`Server is listening on port ${port}...`)
       );
--- models/customer.js
+++ models/customer.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const isEmail = require("validator")
 
 const customerSchema = new mongoose.Schema({
     name:{
