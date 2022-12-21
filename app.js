ba569221e3bd931985007499b8b46065392ce5ed
eshen22 <cb010517@students.apiit.lk>
2022-12-21 10:54:35 +0530

    added new folder structure

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const app = express()
 
 app.use(express.json())
 
-app.listen(5000,(req,res) => console.log("server has started"))
+app.listen(5000,(req,res) => console.log("server has startedhh"))
 
