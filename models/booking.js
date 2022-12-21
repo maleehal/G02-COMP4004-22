ab8a13a7d4fefd7348f14f23882ddb22168b4ba2
maleehal <cb009145@students.apiit.lk>
2022-12-21 22:54:05 +0530

    booking schema

--- models/booking.js
+++ models/booking.js
@@ -1,1 +1,25 @@
-console.log("hi")
+const { time } = require("console")
+const mongoose = require("mongoose")
+const {isEmail} = require("validator")
+
+const bookingSchema = new mongoose.Schema({
+    s_id: {
+        type: mongoose.SchemaType.ObjectId,
+        ref: "customer"
+    },
+    c_id: {
+        type: mongoose.SchemaType.ObjectId,
+        ref: "service-provider"
+    },
+    Status: {
+        type: String
+    },
+    Date: {
+        type: Date
+    },
+    Time: {
+        type: Date
+    }
+})
+
+module.exports = mongoose.model("booking",bookingSchema)
