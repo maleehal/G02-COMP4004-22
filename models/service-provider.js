27fa192f01c3017fb75fae99e5fa4d7fd7d94019
KavinduK <cb010103@students.apiit.lk>
2022-12-22 03:57:58 +0530

    Service provider schema

---
+++ models/service-provider.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose")
+const {isEmail} = require("validator")
+
+
+const service_providerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,"This field is required."],
+
+    },
+
+    email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true,"This field is required."],
+        validate: [isEmail,"Please enter a valid Email."]
+
+    },
+
+    telephone: {
+        type: Number,
+        unique: true,
+        min: 10,
+        max: 10,
+        required: [true, "Please enter a phone number."]
+
+    },
+
+    pimg: {
+        type: Buffer,
+    },
+
+    expertise: {
+
+    },
+
+    username: {
+        type: String,
+        required: [true,"This field is required."],
+
+    },
+
+    password: {
+        type: String,
+        required:[true,"This field is required"]
+    },
+
+    description: {
+        type: String,
+        required: [true,"This field is required"]
+    },
+    
+    about: {
+        type: String,
+        required: [true,"This field is required"]
+    },
+    
+    status: {
+        type: String,
+        default: {type: String, default: "pending"},
+    },
+})
+
+module.exports = mongoose.model("Service_provider",service_providerSchema)
